8934af46548f00edbcfbe129005b5fe8a048ab08
julia <julia.mejia@mail.escuelaing.edu.co>
2023-12-07 08:54:16 -0500

    cors

--- src/main/java/edu/eci/arsw/SketchsyncWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/SketchsyncWebSocketConfig.java
@@ -1,23 +1,34 @@
 package edu.eci.arsw;
 
+import java.util.Arrays;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@SpringBootApplication
 @Configuration
-@EnableWebSocketMessageBroker
-public class SketchsyncWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
-
+@EnableWebSecurity
+public class SketchsyncWebSocketConfig extends WebSecurityConfigurerAdapter {
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://sketchsync.azurewebsites.net/")); // Reemplaza con la URL de tu app React
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
